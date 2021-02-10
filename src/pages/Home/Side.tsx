adced4375a45bed3c3c9962611059eb86eb62051
Giovanni Costa <cgiovanni1996@gmail.com>
2021-02-10 11:22:48 +0100

    :sparkles: improved side form

--- src/pages/Home/Side.tsx
+++ src/pages/Home/Side.tsx
@@ -9,11 +9,13 @@ const Side: FC = ({}) => {
 				<Type>Full Time</Type>
 				<Location>
 					<Label>LOCATION</Label>
-					<LocationInput />
+					<LocationInput placeholder={'City, state, zip code or country'} />
 				</Location>
 				<LocationList>
-					<Radio type="radio" />
-					<RadioLabel>City</RadioLabel>
+					<ListElement>
+						<Radio type="radio" />
+						<RadioLabel>City</RadioLabel>
+					</ListElement>
 				</LocationList>
 			</Form>
 		</Base>
@@ -30,11 +32,44 @@ const Base = styled.div`
 	}
 `
 
-const Form = styled.form``
+const Form = styled.form`
+	font-family: ${({ theme }) => theme.typo.family.main};
+	font-size: ${({ theme }) => theme.typo.size.medium};
+	& > * {
+		margin: 1.2rem 0;
+	}
+`
 const Type = styled.div``
 const Location = styled.div``
-const Label = styled.div``
-const LocationInput = styled.input``
-const LocationList = styled.div``
+const Label = styled.div`
+	color: ${({ theme }) => theme.palette.text.secondary};
+	font-weight: ${({ theme }) => theme.typo.weight.bold};
+	margin-bottom: 0.5rem;
+`
+const LocationInput = styled.input`
+	width: 90%;
+	border: none;
+	padding: 1rem 0;
+	padding-left: 2rem;
+	border-radius: 0.5rem;
+	box-shadow: 0 2px 4px rgba(0, 0, 0, 0.05);
+
+	&::placeholder {
+		color: ${({ theme }) => theme.palette.text.secondary};
+	}
+`
+const LocationList = styled.div`
+	display: flex;
+	flex-direction: column;
+`
+
+const ListElement = styled.div`
+	display: flex;
+	align-items: end;
+	padding: 0.2rem 0;
+`
+
 const Radio = styled.input``
-const RadioLabel = styled.label``
+const RadioLabel = styled.label`
+	margin-left: 0.5rem;
+`
