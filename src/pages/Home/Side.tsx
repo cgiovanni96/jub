0e14cf83f45a551fd17804ea65c472d785d65999
Giovanni Costa <cgiovanni1996@gmail.com>
2021-03-02 09:48:01 +0100

    :sparkles: side form

--- src/pages/Home/Side.tsx
+++ src/pages/Home/Side.tsx
@@ -14,11 +14,19 @@ type FormData = {
 	location: string
 }
 
+type LocationFormData = {
+	city: string
+}
+
 const Side: FC = ({}) => {
 	const { register, handleSubmit } = useForm<FormData>()
+	const {
+		register: registerLocation,
+		handleSubmit: handleSubmitLocation
+	} = useForm<LocationFormData>()
 	const { query, setQuery } = useContext(QueryContext)
 
-	const [cities] = useState<City[]>([
+	const [cities, setCities] = useState<City[]>([
 		{ id: 'london', name: 'London' },
 		{ id: 'amstedam', name: 'Amsterdam' },
 		{ id: 'new+york', name: 'New York' },
@@ -29,39 +37,56 @@ const Side: FC = ({}) => {
 		if (setQuery)
 			setQuery({ location, type: !!type, description: query.description })
 	})
+
+	const onSubmitLocation = handleSubmitLocation(({ city }) => {
+		const cityId = city.toLowerCase().replaceAll(' ', '+')
+		setCities([...cities, { id: cityId, name: city }])
+	})
+
+	console.log('cities', cities)
+
 	return (
 		<Base>
-			<Form onSubmit={onSubmit}>
-				<Type>
-					<Check type="checkbox" id="type" name="type" ref={register} />
-					<CheckLabel htmlFor="type">Full Time</CheckLabel>
-				</Type>
+			<Form id="form" onSubmit={onSubmit}></Form>
+			<LocationForm
+				id="locationForm"
+				onSubmit={onSubmitLocation}
+			></LocationForm>
 
-				<Location>
-					<Label>LOCATION</Label>
+			<Type>
+				<Check type="checkbox" id="type" name="type" ref={register} />
+				<CheckLabel htmlFor="type">Full Time</CheckLabel>
+			</Type>
+
+			<Location>
+				<Label>LOCATION</Label>
+				<LocationWrapper>
 					<LocationInput
 						placeholder={'City, state, zip code or country'}
 						name="city"
 						id="city"
 						defaultValue=""
+						ref={registerLocation}
 					/>
-				</Location>
-				<LocationList>
-					{cities.map((city, id) => (
-						<ListElement key={id}>
-							<Radio
-								type="radio"
-								name="location"
-								value={city.id}
-								ref={register}
-							/>
-							<RadioLabel htmlFor={city.id}>{city.name}</RadioLabel>
-						</ListElement>
-					))}
-				</LocationList>
 
-				<input type="submit" value="submit" />
-			</Form>
+					<LocationButton type="submit" value="+" form={'locationForm'} />
+				</LocationWrapper>
+			</Location>
+			<LocationList>
+				{cities.map((city, id) => (
+					<ListElement key={id}>
+						<Radio
+							type="radio"
+							name="location"
+							value={city.id}
+							ref={register}
+						/>
+						<RadioLabel htmlFor={city.id}>{city.name}</RadioLabel>
+					</ListElement>
+				))}
+			</LocationList>
+
+			<FormButton type="submit" value="Submit" form={'form'} />
 		</Base>
 	)
 }
@@ -74,16 +99,13 @@ const Base = styled.div`
 	${up('sm')} {
 		flex: 2;
 	}
-`
-
-const Form = styled.form`
 	font-family: ${({ theme }) => theme.typo.family.main};
 	font-size: ${({ theme }) => theme.typo.size.medium};
-	& > * {
-		margin: 1.2rem 0;
-	}
 `
+
+const Form = styled.form``
 const Type = styled.div`
+	margin: 1rem 0;
 	display: flex;
 	align-items: center;
 `
@@ -95,11 +117,32 @@ const CheckLabel = styled.label`
 `
 
 const Location = styled.div``
+
+const LocationForm = styled.form`
+	/* display: flex; */
+`
+
+const LocationButton = styled.input`
+	margin: 0 1rem;
+	color: ${({ theme }) => theme.palette.text.secondary};
+	background: ${({ theme }) => theme.palette.accent.default};
+	border: none;
+	padding: 0 1.5rem;
+	border-radius: 0.5rem;
+	box-shadow: 0 2px 4px rgba(0, 0, 0, 0.05);
+	cursor: pointer;
+`
+
 const Label = styled.div`
 	color: ${({ theme }) => theme.palette.text.secondary};
 	font-weight: ${({ theme }) => theme.typo.weight.bold};
 	margin-bottom: 0.5rem;
 `
+
+const LocationWrapper = styled.div`
+	display: flex;
+`
+
 const LocationInput = styled.input`
 	width: 90%;
 	border: none;
@@ -113,8 +156,8 @@ const LocationInput = styled.input`
 	}
 `
 const LocationList = styled.div`
+	margin: 1rem 0;
 	display: flex;
-
 	flex-direction: column;
 `
 
@@ -127,4 +170,15 @@ const ListElement = styled.div`
 const Radio = styled.input``
 const RadioLabel = styled.label`
 	margin-left: 0.5rem;
+`
+
+const FormButton = styled.input`
+	margin: 0 2rem;
+	color: ${({ theme }) => theme.palette.text.primary};
+	background: ${({ theme }) => theme.palette.accent.default};
+	border: none;
+	padding: 1rem 0;
+	border-radius: 0.5rem;
+	box-shadow: 0 2px 4px rgba(0, 0, 0, 0.05);
+	cursor: pointer;
 `
