5fd9ce777e0eb0c7335cb3951219cd83a2345dba
Giovanni Costa <cgiovanni1996@gmail.com>
2021-03-02 11:03:38 +0100

    :sparkles: single job basic layout

--- src/pages/Job/Job.tsx
+++ src/pages/Job/Job.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react'
 import { useQuery } from 'react-query'
-import { useParams } from 'react-router-dom'
+import { useParams, Link as RouterLink } from 'react-router-dom'
 import { JobType } from '../../utility'
 import jobQuery from '../../queries/job'
 import styled from 'styled-components'
@@ -14,9 +14,139 @@ const Job: FC = ({}) => {
 	if (isLoading) return <div>Loading...</div>
 	if (error || !job) return <div>Something went wrong...</div>
 
-	return <Base>{job.title}</Base>
+	return (
+		<Base>
+			<Side>
+				<Back>
+					<RouterLink to="/">Back to search </RouterLink>
+				</Back>
+				<Info>
+					<span>HOW TO APPLY</span>
+					<div>{job.how_to_apply} </div>
+				</Info>
+			</Side>
+
+			<Main>
+				<Header>
+					<TitleSection>
+						<Row>
+							<Title>{job.title}</Title>
+							<Type>{job.type}</Type>
+						</Row>
+						<Time>Time</Time>
+					</TitleSection>
+					<CompanySection>
+						<Logo src={'./logo.jpg'} alt={'logo'} />
+						<Col>
+							<Company>{job.company}</Company>
+							<Location>{job.location}</Location>
+						</Col>
+					</CompanySection>
+				</Header>
+
+				<Description>{job.description}</Description>
+			</Main>
+		</Base>
+	)
 }
 
 export default Job
 
-const Base = styled.div``
+const Base = styled.div`
+	flex: 1;
+	width: 100%;
+	display: flex;
+	font-family: ${({ theme }) => theme.typo.family.accent};
+`
+
+const Side = styled.aside`
+	flex: 1;
+`
+
+const Back = styled.span`
+	& a {
+		text-decoration: none;
+		color: ${({ theme }) => theme.palette.accent.primary};
+	}
+`
+
+const Info = styled.div`
+	margin-top: 1rem;
+
+	& > span {
+		display: inline-block;
+		margin: 1rem 0;
+		color: ${({ theme }) => theme.palette.text.secondary};
+		font-weight: ${({ theme }) => theme.typo.weight.bold};
+	}
+`
+
+const Main = styled.main`
+	flex: 3;
+	font-family: ${({ theme }) => theme.typo.family.main};
+`
+
+const Header = styled.header`
+	display: flex;
+	flex-direction: column;
+`
+
+const TitleSection = styled.div`
+	display: flex;
+	flex-direction: column;
+`
+
+const Row = styled.div`
+	padding: 0.5rem 0;
+	display: flex;
+	align-items: center;
+	font-weight: ${({ theme }) => theme.typo.weight.bold};
+`
+
+const CompanySection = styled.div`
+	margin: 1rem 0;
+	display: flex;
+`
+
+const Logo = styled.img``
+
+const Col = styled.div`
+	margin-left: 1rem;
+	display: flex;
+	flex-direction: column;
+`
+
+const Title = styled.h2`
+	color: ${({ theme }) => theme.palette.text.title};
+	font-size: ${({ theme }) => theme.typo.size.title};
+`
+
+const Type = styled.span`
+	display: inline-block;
+	padding: 0.3rem;
+	margin-left: 1rem;
+	border: 1px solid ${({ theme }) => theme.palette.accent.secondary};
+	border-radius: 0.3rem;
+	font-size: ${({ theme }) => theme.typo.size.small};
+`
+
+const Time = styled.span`
+	color: ${({ theme }) => theme.palette.text.secondary};
+	font-size: ${({ theme }) => theme.typo.size.small};
+`
+
+const Company = styled.h3`
+	color: ${({ theme }) => theme.palette.text.title};
+	font-size: ${({ theme }) => theme.typo.size.big};
+	font-weight: ${({ theme }) => theme.typo.weight.bold};
+`
+
+const Location = styled.span`
+	color: ${({ theme }) => theme.palette.text.secondary};
+	font-size: ${({ theme }) => theme.typo.size.small};
+`
+
+const Description = styled.div`
+	color: ${({ theme }) => theme.palette.accent.secondary};
+	font-size: ${({ theme }) => theme.typo.size.medium};
+`
